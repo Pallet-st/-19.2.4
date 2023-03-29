e7d08026fea66056ce992bf7011f18d7fd256b24
Pallet-st <120416426+Pallet-st@users.noreply.github.com>
2023-03-29 23:20:55 +0300

    Add files via upload

---
+++ app/calc.py
@@ -0,0 +1,12 @@
+class Calculator:
+   def mult(self, x, y):
+       return x * y
+
+   def div(self, x, y):
+       return x / y
+
+   def sub(self, x, y):
+       return x - y
+
+   def add(self, x, y):
+       return x + y
